cc4175d98e4eb4ea59f6b1334552fdc121db745e
Fenoms <xiangwei.wang1991@gmail.com>
2018-01-29 17:06:25 +0900

    update

--- resnetmodel.py
+++ resnetmodel.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 
 
 import tensorflow as tf 
-from transformer import spatial_transformer_network as stn
+# from transformer import spatial_transformer_network as stn
 
 
 _BATCH_NORM_DECAY = 0.997
@@ -301,26 +301,26 @@ def miniImagenet_resnet_v2_generator(block_fn, layers, num_classes, data_format 
 
 
         #localize network to generate the transformation parameters
-        raw_inputs = inputs
+        # raw_inputs = inputs
 
-        inputs = tf.layers.conv2d(inputs = inputs, filters = 32, strides = 2, kernel_size = 5, padding = 'SAME', kernel_initializer=tf.variance_scaling_initializer())
+        # inputs = tf.layers.conv2d(inputs = inputs, filters = 32, strides = 2, kernel_size = 5, padding = 'SAME', kernel_initializer=tf.variance_scaling_initializer())
 
-        print(inputs.shape)
-        inputs = tf.layers.max_pooling2d(inputs = inputs, pool_size = 2, strides = 2, padding = 'VALID')
-        print(inputs.shape)
-        inputs = tf.layers.conv2d(inputs = inputs, filters = 64, strides = 2, kernel_size = 5, padding = 'SAME', kernel_initializer = tf.variance_scaling_initializer())
-        print(inputs.shape)
-        inputs = tf.layers.max_pooling2d(inputs = inputs, pool_size = 2, strides = 2, padding = 'VALID')
-        print(inputs.shape)
-        inputs = tf.layers.dropout(inputs = inputs, rate = _DROPOUT_RATE)
+        # print(inputs.shape)
+        # inputs = tf.layers.max_pooling2d(inputs = inputs, pool_size = 2, strides = 2, padding = 'VALID')
+        # print(inputs.shape)
+        # inputs = tf.layers.conv2d(inputs = inputs, filters = 64, strides = 2, kernel_size = 5, padding = 'SAME', kernel_initializer = tf.variance_scaling_initializer())
+        # print(inputs.shape)
+        # inputs = tf.layers.max_pooling2d(inputs = inputs, pool_size = 2, strides = 2, padding = 'VALID')
+        # print(inputs.shape)
+        # inputs = tf.layers.dropout(inputs = inputs, rate = _DROPOUT_RATE)
 
-        inputs = tf.layers.flatten(inputs = inputs)
+        # inputs = tf.layers.flatten(inputs = inputs)
 
-        inputs = tf.layers.dense(inputs = inputs, units = 128)
-        print(inputs.shape)
-        trans_parameters = tf.layers.dense(inputs = inputs, units = 6)
-        print(trans_parameters.shape)
-        inputs = stn(input_fmap = raw_inputs, theta = trans_parameters, out_dims = [60, 60])
+        # inputs = tf.layers.dense(inputs = inputs, units = 128)
+        # print(inputs.shape)
+        # trans_parameters = tf.layers.dense(inputs = inputs, units = 6)
+        # print(trans_parameters.shape)
+        # inputs = stn(input_fmap = raw_inputs, theta = trans_parameters, out_dims = [60, 60])
 
 
 
@@ -351,17 +351,22 @@ def miniImagenet_resnet_v2_generator(block_fn, layers, num_classes, data_format 
         inputs = block_layer(inputs = inputs, filters = 128, block_fn = block_fn, blocks = layers[1], strides = 2,
             is_training = is_training, name = 'block_layer2', data_format = data_format)
         print('height:', inputs.shape[1])
-        inputs = block_layer(inputs = inputs, filters = 256, block_fn = block_fn, blocks = layers[2], strides = 2, 
-            is_training = is_training, name = 'block_layer3', data_format = data_format)
-        print('height:', inputs.shape[1])
+        # inputs = block_layer(inputs = inputs, filters = 256, block_fn = block_fn, blocks = layers[2], strides = 2, 
+        #     is_training = is_training, name = 'block_layer3', data_format = data_format)
+        # print('height:', inputs.shape[1])
         # inputs = block_layer(inputs = inputs, filters = 512, block_fn = block_fn, blocks = layers[3], strides = 2, 
         #     is_training = is_training, name = 'block_layer4', data_format = data_format)
+
+        inputs = tf.layers.conv2d(inputs = inputs, filters = 256, kernel_size = 3, strides = 2, padding = 'SAME', 
+        		kernel_initializer = tf.variance_scaling_initializer())
 
         print('height:', inputs.shape)
         inputs = batch_norm_relu(inputs, is_training, data_format)
         
         inputs = tf.layers.average_pooling2d(inputs = inputs, pool_size = 3, strides = 1, padding = 'VALID', data_format = data_format)
 
+
+        print('height:', inputs.shape)
         inputs = tf.layers.dropout(inputs = inputs, rate = _DROPOUT_RATE)
 
         inputs = tf.identity(inputs, 'final_avg_pool')
